16091cf3c5e0ed58f518d7e74bdfc460fb01e3cd
valentinavillegasc <116031609+valentinavillegasc@users.noreply.github.com>
2023-07-18 18:59:15 -0500

    color palette and opacity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'primary': '#5691FC',
+        primary: "#5691FC",
+        secondary: "#65E0C7",
+        thirth: "#FFFEC3",
+        grayLight: "#EDEDED",
+        grayLightMed: "#B0B0B0",
+      },
+      opacity: {
+        10: "45%",
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
