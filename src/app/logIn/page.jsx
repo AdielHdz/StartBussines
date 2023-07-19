f2a0a3d9b67902ee9ae293941b6e161ef9c9a0ca
valentinavillegasc <116031609+valentinavillegasc@users.noreply.github.com>
2023-07-18 21:02:16 -0500

    arreglo estilos

--- src/app/logIn/page.jsx
+++ src/app/logIn/page.jsx
@@ -1,7 +1,10 @@
 "use client";
 import React from "react";
 import { useState } from "react";
+import Image from "next/image";
 import validation from "./validations/validations";
+import Background from "public/asset/login.jpg";
+import Authentication from "@/Components/Authentication/Authentication";
 
 export default function LogIn() {
   const [form, setForm] = useState({
@@ -20,97 +23,65 @@ export default function LogIn() {
     setErrors(validation({ ...form, [property]: value }));
   };
   return (
-    <div
-      className="flex justify-center items-center h-screen bg-no-repeat bg-cover bg-center"
-      style={{
-        backgroundImage:
-          "url('https://images.pexels.com/photos/5055748/pexels-photo-5055748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
-      }}>
-      <div className="bg-black bg-opacity-30 p-5 rounded">
-        <div className=" flex items-center justify-center">
-          <div className="container mx-8 my-20">
-            <h1 className="text-white text-center text-6xl mb-10">Logo</h1>
+    <div className="h-screen w-full   pt-0.5">
+      <div className=" absolute z-10 w-full h-full bg-black opacity-10"></div>
+      <Image
+        src={Background}
+        alt="background"
+        width={0}
+        height={0}
+        className="absolute h-full object-cover"
+      />
+      <div
+        className="relative z-20
+            ">
+        <div className="flex justify-center items-center h-screen ">
+          <div className="bg-black bg-opacity-30 p-5 rounded border border-white">
+            <div className=" flex items-center justify-center">
+              <div className="container mx-8 my-20">
+                <h1 className="text-white text-center text-6xl mb-10">Logo</h1>
+              </div>
+            </div>
+
+            <form>
+              <div className="flex flex-col mt-3">
+                <label htmlFor="email" className="text-white">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  className={`bg-black bg-opacity-30 p-2 border ${
+                    error.email ? "border-red-500" : "border-white"
+                  } mt-3  text-white`}
+                />
+                {error.email && <p className="text-red-500">{error.email}</p>}
+              </div>
+
+              <div className="flex flex-col mt-3">
+                <label htmlFor="password" className="text-white">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  onChange={handleChange}
+                  className="bg-black bg-opacity-30 p-2 border border-white  text-white "
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                data-te-ripple-init
+                data-te-ripple-color="light">
+                Login
+              </button>
+              <Authentication />
+            </form>
           </div>
         </div>
-        <div className=" my-5 flex justify-around text-xl">
-          <h1>Logo</h1>
-          <p>/</p>
-          <h1 className="text-2xl underline">Sign in</h1>
-        </div>
-
-        <form>
-          <div className="relative mb-6" data-te-input-wrapper-init>
-            <input
-              type="email"
-              name="email"
-              className={`peer block min-h-[auto] w-full rounded border-0 bg-slate-400 bg-opacity-50 px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 ${
-                form.email
-                  ? "peer-data-[te-input-state-active]:translate-y-[-1.15rem] peer-data-[te-input-state-active]:scale-75"
-                  : ""
-              }`}
-              placeholder="Email"
-              onChange={handleChange}
-            />
-            <label
-              name="email"
-              className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out ${
-                form.email ? "text-primary" : ""
-              }`}></label>
-            {error.email && <p>{error.email}</p>}
-          </div>
-
-          <div className="relative mb-6" data-te-input-wrapper-init>
-            <input
-              type="password"
-              className={`peer block min-h-[auto] w-full rounded border-0 bg-slate-400 bg-opacity-50 px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 ${
-                form.password
-                  ? "peer-data-[te-input-state-active]:translate-y-[-1.15rem] peer-data-[te-input-state-active]:scale-75"
-                  : ""
-              }`}
-              name="password"
-              placeholder="Password"
-              onChange={handleChange}
-            />
-            <label
-              name="password"
-              className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out ${
-                form.password ? "text-primary" : ""
-              }`}></label>
-            {error.password && <p>{error.password}</p>}
-          </div>
-
-          <button
-            type="submit"
-            className="inline-block w-full rounded bg-indigo-500 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-            data-te-ripple-init
-            data-te-ripple-color="light">
-            Log In
-          </button>
-          <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-            <p className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
-              OR
-            </p>
-          </div>
-
-          <div className="flex justify-center items-center ">
-            <a className="mb-3 flex w-1/4 items-center justify-center rounded bg-red-500 px-5 pb-2 pt-2.5 text-center text-xs font-medium uppercase leading-normal text-white shadow-sm transition duration-150 ease-in-out hover:bg-red-600 hover:shadow-md focus:bg-red-600 focus:shadow-md focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-md dark:shadow-sm dark:hover:shadow-md dark:focus:shadow-md dark:active:shadow-md">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="mr-2 h-3 w-3"
-                fill="currentColor"
-                viewBox="0 0 24 24"></svg>
-            </a>
-            <a className="mb-3 flex w-1/4 items-center justify-center rounded bg-blue-500 px-5 pb-2 pt-2.5 text-center text-xs font-medium uppercase leading-normal text-white shadow-sm transition duration-150 ease-in-out hover:bg-blue-600 hover:shadow-md focus:bg-blue-600 focus:shadow-md focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-md dark:shadow-sm dark:hover:shadow-md dark:focus:shadow-md dark:active:shadow-md">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="mr-2 h-3 w-3"
-                fill="currentColor"
-                viewBox="0 0 24 24">
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-              </svg>
-            </a>
-          </div>
-        </form>
       </div>
     </div>
   );
