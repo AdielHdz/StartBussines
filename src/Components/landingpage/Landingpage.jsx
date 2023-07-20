4fd2badcf4e5dc3e1b2c0303a4e877628f1542e7
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-07-19 23:23:44 -0300

    Modularización de botone y mejora de estilos

--- src/Components/landingpage/Landingpage.jsx
+++ src/Components/landingpage/Landingpage.jsx
@@ -1,13 +1,29 @@
-import Link from "next/link";
 import "../../../src/app/globals.css";
 import InversionistaCard from "./Cards/InversionistaCard";
 import EmprendedorCard from "./Cards/EmprendedorCard";
+import Button from "./Cards/Button/Button"; 
+
+const BACKGROUND_IMAGE_URL =
+  "https://images.unsplash.com/photo-1661956602153-23384936a1d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80";
+
+const TEXT = {
+  title: "DEAL UP!",
+  description: `
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+    nam animi dolore tempore ab aspernatur consectetur provident. Facere
+    laudantium modi minima ratione quae, sed id. Iusto assumenda dolorem
+    magni veniam? Lorem ipsum dolor sit, amet consectetur adipisicing
+    elit. Reiciendis, vitae quasi vel ut exercitationem sit inventore
+    laudantium ducimus voluptatem nulla quis repudiandae porro officiis
+    dolores, sed dolorem tenetur at? Illum.
+  `,
+};
+
 const LandingPage = () => (
   <div
     className="h-screen w-screen bg-center"
     style={{
-      backgroundImage:
-        "url('https://images.unsplash.com/photo-1661956602153-23384936a1d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80')",
+      backgroundImage: `url(${BACKGROUND_IMAGE_URL})`,
       backgroundSize: "cover",
       backgroundPosition: "center",
       height: "100%",
@@ -16,22 +32,16 @@ const LandingPage = () => (
   >
     <div className=" flex items-center justify-center">
       <div className="container mx-4 md:mx-8 my-20 flex items-center justify-center">
-        <h1 className="text-4xl md:text-6xl text-center text-black mb-10">
-          Logo
+        <h1 className="text-3xl md:text-6xl text-center text-black mb-10">
+          {TEXT.title}
         </h1>
       </div>
     </div>
 
-    <div className="flex flex-col items-center justify-center  text-white px-4 md:px-8 py-20">
+    <div className="flex flex-col items-center justify-center text-white px-4 md:px-8 py-20">
       <div className="container mb-10">
-        <p className="text-lg text-black leading-relaxed text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-          nam animi dolore tempore ab aspernatur consectetur provident. Facere
-          laudantium modi minima ratione quae, sed id. Iusto assumenda dolorem
-          magni veniam? Lorem ipsum dolor sit, amet consectetur adipisicing
-          elit. Reiciendis, vitae quasi vel ut exercitationem sit inventore
-          laudantium ducimus voluptatem nulla quis repudiandae porro officiis
-          dolores, sed dolorem tenetur at? Illum.
+        <p className="text-sm text-black leading-relaxed text-center mx-4">
+          {TEXT.description}
         </p>
       </div>
       <EmprendedorCard />
@@ -39,16 +49,8 @@ const LandingPage = () => (
     </div>
 
     <div className="bottom-0 left-0 w-full flex justify-center items-center bg-primary p-4">
-      <Link href="/logIn">
-        <button className="btn bg-secondary hover:bg-primary hover:text-black text-white font-bold py-2 md:py-3 px-4 md:px-6 rounded-lg m-2 text-lg transition-all duration-200">
-          Login
-        </button>
-      </Link>
-      <Link href="/register">
-        <button className="btn bg-secondary hover:bg-primary hover:text-black text-white font-bold py-2 md:py-3 px-4 md:px-6 rounded-lg m-2 text-lg transition-all duration-200">
-          Registro
-        </button>
-      </Link>
+      <Button text="LOGIN" href="/logIn" />
+      <Button text="SIGN IN" href="/register" />
     </div>
   </div>
 );
