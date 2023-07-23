f03bd0d80d9f3a00e6ddcf8023694ef98446d151
valentinavillegasc <116031609+valentinavillegasc@users.noreply.github.com>
2023-07-23 13:33:24 -0500

    configuracion para edición del usuario

--- src/Redux/Fetching/UsersSlice/UserSlice.js
+++ src/Redux/Fetching/UsersSlice/UserSlice.js
@@ -1,48 +1,54 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
+import axios from "axios";
 const users = [
-    {id: 1, name: "Daniel", age: 25 },
-    {id: 2, name: "Valentina", age: 20 },
-    {id: 3, name: "Adiel", age: 23 },
-    {id: 4, name: "Mauroo", age: 28 }
-]
-
-
+  { id: 1, name: "Daniel", age: 25 },
+  { id: 2, name: "Valentina", age: 20 },
+  { id: 3, name: "Adiel", age: 23 },
+  { id: 4, name: "Mauroo", age: 28 },
+];
 
 export const getAllUsers = createAsyncThunk("getAllUsers", async () => {
-    console.log("Funcionando correctamente")
-    return await fetch("https://reqres.in/api/users")
-    .then(response => response.json())
-    .then(data => data.data)
-})
-
-
-const User = createSlice({
-    name: "user",
-    initialState: {
-        user: {},
-        users: users,
-        usersFilter: users,
-    },
-    reducers: {
-        filterByAge: (state, action) => {
-            state.usersFilter = state.users.filter(user => user.age > action.payload)
-        },
-        filterById: (state, action) => {
-
-        }
-    },
-    extraReducers: (builder) => {
-        builder
-        .addCase(getAllUsers.fulfilled, (state, action) => {
-            state.users = action.payload
-            state.usersFilter = action.payload
-        });
-    }
+  console.log("Funcionando correctamente");
+  return await fetch("https://reqres.in/api/users")
+    .then((response) => response.json())
+    .then((data) => data.data);
 });
 
+export const getUserById = createAsyncThunk("getUserById", async (id) => {
+  const response = await axios.get(`http://localhost:3001/users/${id}`);
+  return response.data[0];
+});
 
-export const { filterByAge } = User.actions;
+const User = createSlice({
+  name: "user",
+  initialState: {
+    user: {},
+    userDetail: {},
+    users: users,
+    usersFilter: users,
+  },
+  reducers: {
+    filterByAge: (state, action) => {
+      state.usersFilter = state.users.filter(
+        (user) => user.age > action.payload
+      );
+    },
+    filterById: (state, action) => {},
+    updateUser: (state, action) => {
+      state.userDetail = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllUsers.fulfilled, (state, action) => {
+        state.users = action.payload;
+        state.usersFilter = action.payload;
+      })
+      .addCase(getUserById.fulfilled, (state, action) => {
+        state.userDetail = action.payload;
+      });
+  },
+});
+
+export const { filterByAge, updateUser } = User.actions;
 export default User;
-
-
--- src/app/userProfile/page.jsx
+++ src/app/userProfile/page.jsx
@@ -1,57 +1,74 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Background from "public/asset/login.jpg";
 import { GrAdd } from "react-icons/gr";
 import { AiOutlineEdit } from "react-icons/ai";
 import { AiFillEdit } from "react-icons/ai";
+import UserInfo from "./UserInfo";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "next/navigation";
+import { getUserById } from "../../Redux/Fetching/UsersSlice/UserSlice";
+import axios from "axios";
+import { updateUser } from "../../Redux/Fetching/UsersSlice/UserSlice";
 
 export default function UserProfile() {
-  const [userData, setUserData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    birthdate: "",
-    address: "",
-    /* avatar: null,
-    frontId: null,
-    backId: null, */
-  });
+  const user = useSelector((state) => state.user.userDetail);
+
+  const params = useParams();
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({});
+
   const [inputsDisabled, setInputsDisabled] = useState(true);
+  const [changesSaved, setChangesSaved] = useState(true);
+
+  useEffect(() => {
+    dispatch(getUserById("e0b214e4-b719-48d7-9570-5f3e6b53a4a3"));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      setUserData({
+        name: user.name,
+        email: user.email,
+        rol: user.rol,
+        birthdate: user.birthdate,
+        phone: user.phone,
+        address: user.address,
+      });
+    }
+  }, [user]);
 
   const handleChange = (event) => {
     const property = event.target.name;
     const value = event.target.value;
     setUserData({ ...userData, [property]: value });
+    setChangesSaved(false);
   };
   const toggleInputs = (event) => {
     event.preventDefault();
     setInputsDisabled(!inputsDisabled);
+    if (changesSaved) {
+      setInputsDisabled(!inputsDisabled);
+    }
   };
 
-  /* const handleAvatarChange = (event) => {
-    const file = event.target.files[0];
-    setUserData({ ...userData, avatar: file });
-  };
-  const handleFrontIdChange = (event) => {
-    const file = event.target.files[0];
-    setUserData({ ...userData, frontId: file });
-  };
-  const handleBackIdChange = (event) => {
-    const file = event.target.files[0];
-    setUserData({ ...userData, backIdId: file });
-  };
-
-  const handleSubmit = (event) => {
+  const handleSaveChanges = (event) => {
     event.preventDefault();
 
-    const formData = new FormData();
-    formData.append("name", userData.name);
-    formData.append("email", userData.email);
-    formData.append("phone", userData.phone);
-    formData.append("birthdate", userData.birthdate);
-    formData.append("address", userData.address);
-  }; */
+    axios
+      .put(
+        "http://localhost:3001/users/e0b214e4-b719-48d7-9570-5f3e6b53a4a3",
+        userData
+      )
+      .then((res) => {
+        alert("Edited!");
+        setInputsDisabled(true);
+        setChangesSaved(true);
+        dispatch(updateUser(userData));
+      })
+      .catch((err) => console.log(err.response.data));
+  };
 
   return (
     <div className="flex flex-col items-center h-full w-full pt-0.5">
@@ -75,117 +92,134 @@ export default function UserProfile() {
         <div className="justify-center items-center h-screen w-96 max-lg:w-110 p-4">
           <h1>User data</h1>
           <hr className="border-black" />
-          <form action="">
-            <div className="flex flex-col mt-3">
-              <label htmlFor="name" className="text-labelRed">
-                Name
-              </label>
-              <input
-                type="text"
-                className="rounded bg-grayLight h-10"
-                onChange={handleChange}
-                disabled={inputsDisabled}
-              />
-            </div>
 
-            <div className="flex flex-col mt-3">
-              <label htmlFor="email" className="text-labelRed">
-                Email
-              </label>
-              <input
-                type="email"
-                className="rounded bg-grayLight h-10"
-                onChange={handleChange}
-                disabled={inputsDisabled}
-              />
-            </div>
-            <div className="flex flex-col mt-3">
-              <label htmlFor="birthdate" className="text-labelRed">
-                Birthdate
-              </label>
-              <input
-                type="date"
-                className="rounded bg-grayLight h-10"
-                onChange={handleChange}
-                disabled={inputsDisabled}
-              />
-            </div>
-            <div>
-              <h1 className="mt-5 mb-2">Important data</h1>
-              <hr className="border-black" />
-            </div>
-            <div className="flex flex-col mt-3">
-              <label htmlFor="phone" className="text-labelRed">
-                Phone
-              </label>
-              <input
-                type="addres"
-                className="rounded bg-grayLight h-10"
-                onChange={handleChange}
-                disabled={inputsDisabled}
-              />
-            </div>
-            <div className="flex flex-col mt-3">
-              <label htmlFor="address" className="text-labelRed">
-                Address
-              </label>
-              <input
-                type="addres"
-                className="rounded bg-grayLight h-10"
-                onChange={handleChange}
-                disabled={inputsDisabled}
-              />
-            </div>
-            <div className="mt-3">
-              <label htmlFor="" className="text-labelRed ">
-                ID photo
-              </label>
-              <div className="flex flex-col items-center justify-center w-full bg-grayLight p-1">
-                <label
-                  htmlFor="frontIDInput"
-                  className="flex flex-col items-center justify-center w-full h-60 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-grayLight m-3">
-                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <GrAdd className="w-7 h-7" />
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span className="font-semibold">Image</span>
-                    </p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">
-                      Frontal ID
-                    </p>
-                  </div>
-                  <input
-                    id="frontIDInput"
-                    type="file"
-                    className="hidden"
-                    disabled={inputsDisabled}
-                  />
+          {inputsDisabled ? (
+            <UserInfo user={user} />
+          ) : (
+            <form action="">
+              <div className="flex flex-col mt-3">
+                <label htmlFor="name" className="text-labelRed">
+                  Name
                 </label>
-
-                <label
-                  htmlFor="backIDInput"
-                  className="flex flex-col items-center justify-center w-full h-60 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-grayLight m-3">
-                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <GrAdd className="w-7 h-7" />
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span className="font-semibold">Image</span>
-                    </p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">
-                      Back ID
-                    </p>
-                  </div>
-                  <input
-                    id="backIDInput"
-                    type="file"
-                    className="hidden"
-                    disabled={inputsDisabled}
-                  />
-                </label>
+                <input
+                  name="name"
+                  type="text"
+                  className="rounded bg-grayLight h-10"
+                  onChange={handleChange}
+                  disabled={inputsDisabled}
+                  value={userData.name}
+                />
               </div>
-            </div>
-            <button className=" w-full h-10 border text-white bg-greenPrimary rounded mt-2 mb-3">
-              Save changes
-            </button>
-          </form>
+
+              <div className="flex flex-col mt-3">
+                <label htmlFor="email" className="text-labelRed">
+                  Email
+                </label>
+                <input
+                  name="email"
+                  type="email"
+                  className="rounded bg-grayLight h-10"
+                  onChange={handleChange}
+                  disabled={inputsDisabled}
+                  value={userData.email}
+                />
+              </div>
+              <div className="flex flex-col mt-3">
+                <label htmlFor="birthdate" className="text-labelRed">
+                  Birthdate
+                </label>
+                <input
+                  name="birthdate"
+                  type="date"
+                  className="rounded bg-grayLight h-10"
+                  onChange={handleChange}
+                  disabled={inputsDisabled}
+                  value={userData.birthdate}
+                />
+              </div>
+              <div>
+                <h1 className="mt-5 mb-2">Important data</h1>
+                <hr className="border-black" />
+              </div>
+              <div className="flex flex-col mt-3">
+                <label htmlFor="phone" className="text-labelRed">
+                  Phone
+                </label>
+                <input
+                  name="phone"
+                  type="addres"
+                  className="rounded bg-grayLight h-10"
+                  onChange={handleChange}
+                  disabled={inputsDisabled}
+                  value={userData.phone}
+                />
+              </div>
+              <div className="flex flex-col mt-3">
+                <label htmlFor="address" className="text-labelRed">
+                  Address
+                </label>
+                <input
+                  name="address"
+                  type="address"
+                  className="rounded bg-grayLight h-10"
+                  onChange={handleChange}
+                  disabled={inputsDisabled}
+                  value={userData.address}
+                />
+              </div>
+              {/* <div className="mt-3">
+                <label htmlFor="" className="text-labelRed ">
+                  ID photo
+                </label>
+                <div className="flex flex-col items-center justify-center w-full bg-grayLight p-1">
+                  <label
+                    htmlFor="frontIDInput"
+                    className="flex flex-col items-center justify-center w-full h-60 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-grayLight m-3">
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                      <GrAdd className="w-7 h-7" />
+                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Image</span>
+                      </p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">
+                        Frontal ID
+                      </p>
+                    </div>
+                    <input
+                      id="frontIDInput"
+                      type="file"
+                      className="hidden"
+                      disabled={inputsDisabled}
+                    />
+                  </label>
+
+                  <label
+                    htmlFor="backIDInput"
+                    className="flex flex-col items-center justify-center w-full h-60 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-grayLight m-3">
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                      <GrAdd className="w-7 h-7" />
+                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Image</span>
+                      </p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">
+                        Back ID
+                      </p>
+                    </div>
+                    <input
+                      id="backIDInput"
+                      type="file"
+                      className="hidden"
+                      disabled={inputsDisabled}
+                    />
+                  </label>
+                </div>
+              </div> */}
+              <button
+                className=" w-full h-10 border text-white bg-greenPrimary rounded mt-2 mb-3"
+                onClick={handleSaveChanges}>
+                Save changes
+              </button>
+            </form>
+          )}
         </div>
       </div>
     </div>
