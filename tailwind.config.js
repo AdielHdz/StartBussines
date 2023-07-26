11a9b0b33ed4ebb7fd9a3f6b4539befb5cd4814d
Adiel <wereber.dragons14@live.com.mx>
2023-07-25 22:27:22 -0600

    LandingPage finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        whites: "#FFFFFF",
+        blacks: "#000000",
         primar: "#1C997F",
         second: "#5691FC",
         thirth: "#FFFEC3",
