ddfeb8e2a798ecbdcca46200aaba0d6667c02d89
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-07-26 15:50:37 -0300

    Creation Post

--- src/Components/SearchProjectsSuggestions.jsx
+++ src/Components/SearchProjectsSuggestions.jsx
@@ -10,7 +10,8 @@ export const SearchProjectsSuggestions = ({ suggestions, closeSuggestions }) => 
     <a href="/projects" onClick={handleClick}>
       {/* <NavLink to={`/profile/${suggestions.id_users}`} onClick={handleClick}> */}
         <div className="p-10px 20px hover:bg-efefef cursor-pointer">
-          {suggestions.projectName}
+          {suggestions.name}
+          
         </div>
       {/* </NavLink> */}
     </a>
--- src/Redux/Fetching/UsersSlice/UserSlice.js
+++ src/Redux/Fetching/UsersSlice/UserSlice.js
@@ -1,41 +1,40 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 const users = [
-  { id: 1, name: 'Daniel', age: 25 },
-  { id: 2, name: 'Valentina', age: 20 },
-  { id: 3, name: 'Adiel', age: 23 },
-  { id: 4, name: 'Mauroo', age: 28 },
+  { id: 1, name: "Daniel", age: 25 },
+  { id: 2, name: "Valentina", age: 20 },
+  { id: 3, name: "Adiel", age: 23 },
+  { id: 4, name: "Mauroo", age: 28 },
 ];
 
-export const getAllUsers = createAsyncThunk('getAllUsers', async () => {
-  console.log('Funcionando correctamente');
-  return await axios('https://reqres.in/api/users')
+export const getAllUsers = createAsyncThunk("getAllUsers", async () => {
+  console.log("Funcionando correctamente");
+  return await axios("https://reqres.in/api/user")
     .then((response) => response.json())
     .then((data) => data.data);
 });
 
 export const registerUser = createAsyncThunk(
-  'registerUser',
+  "registerUser",
   async (newUser) => {
-    console.log('Datos enviados a la API:', newUser);
+    console.log("Datos enviados a la API:", newUser);
 
     try {
       const response = await axios.post(`http://localhost:3001/user`, newUser);
 
       if (
         response.data.error &&
-
-        response.data.error === 'User already exists'
+        response.data.error === "User already exists"
       ) {
-        throw new Error('User already exists');
+        throw new Error("User already exists");
       }
 
-      console.log('Datos recibidos de la API:', response.data);
+      console.log("Datos recibidos de la API:", response.data);
 
       return response.data;
     } catch (error) {
-      console.log('Failed to register user:', error.response.data);
-      alert (error.response.data.error); 
+      console.log("Failed to register user:", error.response.data);
+      alert(error.response.data.error);
     }
   }
 );
@@ -43,16 +42,42 @@ export const registerUser = createAsyncThunk(
 export const getUserById = createAsyncThunk("getUserById", async (id) => {
   const response = await axios.get(`http://localhost:3001/user/${id}`);
   return response.data;
-
 });
 
+export const getProjectsByName = createAsyncThunk(
+  "getProjectsByName",
+  async (nameProjects) => {
+    console.log(`getProjectsByName se está ejecutando con el nombre: ${nameProjects}`);
+    
+
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/projects?name=${nameProjects}`
+      );
+
+      console.log("Datos recibidos de la API:", response.data);
+
+      if (response.data.error) {
+        throw new Error(response.data.error);
+      }
+
+      return response.data;
+    } catch (error) {
+      console.log(`Failed to get projects: ${error.message}`);
+      return [];
+    }
+  }
+);
+
+
 const User = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {
     user: {},
     userDetail: {},
     users: users,
     usersFilter: users,
+    searchResults: [],
   },
   reducers: {
     filterByAge: (state, action) => {
@@ -65,6 +90,7 @@ const User = createSlice({
       state.userDetail = action.payload;
     },
   },
+
   extraReducers: (builder) => {
     builder
       .addCase(getAllUsers.fulfilled, (state, action) => {
@@ -77,6 +103,9 @@ const User = createSlice({
       })
       .addCase(getUserById.fulfilled, (state, action) => {
         state.userDetail = action.payload;
+      })
+      .addCase(getProjectsByName.fulfilled, (state, action) => {
+        state.searchResults = action.payload;
       });
   },
 });
