27e355f97b07a52aede3efc8968d2c4279d4aba3
Adiel <wereber.dragons14@live.com.mx>
2023-07-26 20:30:41 -0600

    fix error handleClick

--- src/Components/SelectWay/SelectWay.jsx
+++ src/Components/SelectWay/SelectWay.jsx
@@ -1,19 +1,35 @@
 import Image from "next/image";
 import IconEntrepreneur from "./entrepreneurIcon.svg";
-const SelectWay = () => {
-  console.log(IconEntrepreneur);
-  return (
-    <button className="flex flex-col items-center justify-center text-blacks border-2 shadow-cards rounded-xl p-2">
-      <Image
-        src={IconEntrepreneur}
-        alt="icons"
-        width={1000}
-        height={1000}
-        className="w-24  h-28 object-cover fill-red-800 "
-      />
-      <h5 className="text-sm">Entrepreneur</h5>
-    </button>
-  );
+import IconInvestor from "./investorIcon.svg";
+
+const SelectWay = ({ entrepreneur, investor }) => {
+  if (entrepreneur) {
+    return (
+      <button className="flex flex-col items-center justify-center border text-blacks hover:shadow-cards hover:scale-105 transform transition-all duration-200 rounded-xl p-2 w-32">
+        <Image
+          src={IconEntrepreneur}
+          alt="icons"
+          width={1000}
+          height={1000}
+          className="w-24 h-28 object-cover "
+        />
+        <h5 className="text-sm w-full text-center">Entrepreneur</h5>
+      </button>
+    );
+  } else if (investor) {
+    return (
+      <button className="flex flex-col items-center justify-center border text-blacks hover:shadow-cards hover:scale-105 transform transition-all duration-200 rounded-xl w-32  h-40">
+        <Image
+          src={IconInvestor}
+          alt="icons"
+          width={1000}
+          height={1000}
+          className=" w-20 h-20 object-cover "
+        />
+        <h5 className="text-sm w-full text-center">Investor</h5>
+      </button>
+    );
+  }
 };
 
 export default SelectWay;
