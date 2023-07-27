27c8baf14bd813c037835cfca689e20fae541c6d
Adiel <wereber.dragons14@live.com.mx>
2023-07-26 19:05:08 -0600

    Conectela funcionalidad de login

--- src/Components/RegisterForm/RegisterForm.jsx
+++ src/Components/RegisterForm/RegisterForm.jsx
@@ -14,7 +14,7 @@ import Authentication from "../../Components/Authentication/Authentication";
 import NavigationButtons from "../NavigationButtons/NavigationButtons";
 import { useDispatch } from "react-redux";
 import { registerUser } from "../../Redux/Fetching/UsersSlice/UserSlice";
-
+import SelectWay from "../SelectWay/SelectWay";
 const RegisterForm = () => {
   const dispatch = useDispatch();
   const [name, setName] = useState("");
@@ -39,16 +39,16 @@ const RegisterForm = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     const isFormValid =
-    name !== "" &&
-    email !== "" &&
-    dob !== "" &&
-    password !== "" &&
-    confirmPassword !== "";
-  if (!isFormValid) {
-    alert("There are fields that are not completed");
-    return;
-  }
-   
+      name !== "" &&
+      email !== "" &&
+      dob !== "" &&
+      password !== "" &&
+      confirmPassword !== "";
+    if (!isFormValid) {
+      alert("There are fields that are not completed");
+      return;
+    }
+
     if (
       nameError ||
       emailError ||
@@ -218,172 +218,132 @@ const RegisterForm = () => {
   const currentPage = "/register";
 
   return (
-    <div
-      className="flex justify-center items-center min-h-screen bg-no-repeat bg-cover bg-center"
-      style={{
-        backgroundImage:
-          "url('https://images.pexels.com/photos/5055748/pexels-photo-5055748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
-      }}
-    >
-      <div className="p-5 mt-10 mb-10  text-white">
+    <div className="p-2 mt-10 mb-10  text-white border-2 border-green-800">
+      <div className=" p-2 mt-10 mb-10 rounded border-2 border-green-400 text-white">
         <NavigationButtons currentPage={currentPage} />
-        <div className="bg-black bg-opacity-25 p-5 mt-10 mb-10 rounded border border-white text-white">
-          <div className="flex justify-between">
-            {" "}
-            {/* Utilizamos flexbox */}
-            <div className="flex-1 mr-2">
-              {" "}
-              {/* División en dos partes iguales */}
-              <button
-                className={`w-full py-2 px-4 rounded ${
-                  isEntrepreneur
-                    ? "bg-white text-black"
-                    : "bg-gray-300 text-white"
-                }`}
-                onClick={() => setIsEntrepreneur(true)}
-              >
-                Entrepreneur
-              </button>
-            </div>
-            <div className="flex-1 ml-2">
-              {" "}
-              {/* División en dos partes iguales */}
-              <button
-                className={`w-full py-2 px-4 rounded ${
-                  !isEntrepreneur
-                    ? "bg-white text-black"
-                    : "bg-gray-300 text-white"
-                }`}
-                onClick={() => setIsEntrepreneur(false)}
-              >
-                Investor
-              </button>
-            </div>
-          </div>
-          <form onSubmit={onSubmit} className="max-w-md mx-auto">
-            <div className="flex flex-col mt-3">
-              <label htmlFor="name" className="text-white">
-                Full Name
-              </label>
-              <input
-                id="name"
-                type="text"
-                value={name}
-                onChange={onNameChange}
-                onBlur={onNameBlur}
-                className={`bg-black bg-opacity-10 p-2 border ${
-                  nameError ? "border-red-500" : "border-white"
-                } mt-2`}
-              />
-              {nameError && <p className="text-red-500">{nameError}</p>}
-            </div>
-            <div className="flex flex-col mt-3">
-              <label htmlFor="email" className="text-white">
-                Email
-              </label>
-              <input
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                onBlur={onEmailBlur}
-                className={`bg-black bg-opacity-10 p-2 border ${
-                  emailError ? "border-red-500" : "border-white"
-                } mt-3`}
-              />
-              {emailExist && <p className="text-red-500">{emailExist}</p>}
-              {emailError && <p className="text-red-500">{emailError}</p>}
-            </div>
-            <div className="flex flex-col mt-3">
-              <label htmlFor="dob" className="text-white">
-                Date of Birth
-              </label>
-              <input
-                id="dob"
-                type="date"
-                value={dob}
-                onChange={(e) => setDob(e.target.value)}
-                onBlur={onDobBlur}
-                className={`bg-black bg-opacity-10 p-2 border ${
-                  dobError ? "border-red-500" : "border-white"
-                } mt-3`}
-              />
-              {dobError && <p className="text-red-500">{dobError}</p>}
-            </div>
-            <div className="flex flex-col mt-3">
-              <label htmlFor="password" className="text-white">
-                Password
-              </label>
-              <div className="relative">
-                <input
-                  id="password"
-                  type={showPassword ? "text" : "password"}
-                  value={password}
-                  onChange={onPasswordChange}
-                  onBlur={onPasswordBlur}
-                  className={`bg-black bg-opacity-10 p-2 border ${
-                    passwordError ? "border-red-500" : "border-white"
-                  } mt-3 w-full`}
-                />
-                <button
-                  type="button"
-                  onClick={togglePasswordVisibility}
-                  className="absolute right-1 top-8 transform -translate-y-1/2 "
-                >
-                  {showPassword ? (
-                    <AiFillEyeInvisible className="w-6 h-6" />
-                  ) : (
-                    <AiFillEye className="w-6 h-6" />
-                  )}
-                </button>
-              </div>
-              {passwordError && <p className="text-red-500">{passwordError}</p>}
-            </div>
-            <div className="flex flex-col mt-3">
-              <label htmlFor="confirmPassword" className="text-white">
-                Confirm Password
-              </label>
-              <div className="relative">
-                <input
-                  id="confirmPassword"
-                  type={showConfirmPassword ? "text" : "password"}
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  onBlur={onConfirmPasswordBlur}
-                  className={`bg-black bg-opacity-10 p-2 border  ${
-                    confirmPasswordError ? "border-red-500" : "border-white"
-                  } mt-3 w-full`}
-                />
-                <button
-                  type="button"
-                  onClick={toggleConfirmPasswordVisibility}
-                  className="absolute right-1 top-8 transform -translate-y-1/2 "
-                >
-                  {showConfirmPassword ? (
-                    <AiFillEyeInvisible className="w-6 h-6" />
-                  ) : (
-                    <AiFillEye className="w-6 h-6" />
-                  )}
-                </button>
-              </div>
-              {confirmPasswordError && (
-                <p className="text-red-500">{confirmPasswordError}</p>
-              )}
-            </div>
-            <CustomButton
-              text="Register"
-              color="blue"
-              disabled={!isFormValid}
+        <SelectWay />
+        <form
+          onSubmit={onSubmit}
+          className="max-w-md mx-auto border-2 border-purple-500"
+        >
+          <div className="flex flex-col mt-3">
+            <label htmlFor="name" className="text-white">
+              Full Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              value={name}
+              onChange={onNameChange}
+              onBlur={onNameBlur}
+              className={`bg-black bg-opacity-10 p-2 border ${
+                nameError ? "border-red-500" : "border-white"
+              } mt-2`}
             />
-          </form>
-          {successMessage && (
-            <p className="text-green-500 text-center uppercase text-xl">
-              {successMessage}
-            </p>
-          )}
+            {nameError && <p className="text-red-500">{nameError}</p>}
+          </div>
+          <div className="flex flex-col mt-3">
+            <label htmlFor="email" className="text-white">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onBlur={onEmailBlur}
+              className={`bg-black bg-opacity-10 p-2 border ${
+                emailError ? "border-red-500" : "border-white"
+              } mt-3`}
+            />
+            {emailExist && <p className="text-red-500">{emailExist}</p>}
+            {emailError && <p className="text-red-500">{emailError}</p>}
+          </div>
+          <div className="flex flex-col mt-3">
+            <label htmlFor="dob" className="text-white">
+              Date of Birth
+            </label>
+            <input
+              id="dob"
+              type="date"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+              onBlur={onDobBlur}
+              className={`bg-black bg-opacity-10 p-2 border ${
+                dobError ? "border-red-500" : "border-white"
+              } mt-3`}
+            />
+            {dobError && <p className="text-red-500">{dobError}</p>}
+          </div>
+          <div className="flex flex-col mt-3">
+            <label htmlFor="password" className="text-white">
+              Password
+            </label>
+            <div className="relative">
+              <input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={onPasswordChange}
+                onBlur={onPasswordBlur}
+                className={`bg-black bg-opacity-10 p-2 border ${
+                  passwordError ? "border-red-500" : "border-white"
+                } mt-3 w-full`}
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute right-1 top-8 transform -translate-y-1/2 "
+              >
+                {showPassword ? (
+                  <AiFillEyeInvisible className="w-6 h-6" />
+                ) : (
+                  <AiFillEye className="w-6 h-6" />
+                )}
+              </button>
+            </div>
+            {passwordError && <p className="text-red-500">{passwordError}</p>}
+          </div>
+          <div className="flex flex-col mt-3">
+            <label htmlFor="confirmPassword" className="text-white">
+              Confirm Password
+            </label>
+            <div className="relative">
+              <input
+                id="confirmPassword"
+                type={showConfirmPassword ? "text" : "password"}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                onBlur={onConfirmPasswordBlur}
+                className={`bg-black bg-opacity-10 p-2 border  ${
+                  confirmPasswordError ? "border-red-500" : "border-white"
+                } mt-3 w-full`}
+              />
+              <button
+                type="button"
+                onClick={toggleConfirmPasswordVisibility}
+                className="absolute right-1 top-8 transform -translate-y-1/2 "
+              >
+                {showConfirmPassword ? (
+                  <AiFillEyeInvisible className="w-6 h-6" />
+                ) : (
+                  <AiFillEye className="w-6 h-6" />
+                )}
+              </button>
+            </div>
+            {confirmPasswordError && (
+              <p className="text-red-500">{confirmPasswordError}</p>
+            )}
+          </div>
+          <CustomButton text="Register" color="blue" disabled={!isFormValid} />
+        </form>
+        {successMessage && (
+          <p className="text-green-500 text-center uppercase text-xl">
+            {successMessage}
+          </p>
+        )}
 
-          <Authentication />
-        </div>
+        <Authentication />
       </div>
     </div>
   );
