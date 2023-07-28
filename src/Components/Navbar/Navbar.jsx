ac35a98bb54a24069fc6db2af92b3b49e7d95327
valentinavillegasc <116031609+valentinavillegasc@users.noreply.github.com>
2023-07-27 19:59:38 -0500

    homefix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,12 +2,11 @@
 import { Fragment, useEffect } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useLocalStorage } from "../hooks/useLocalStorage";
 import { useRouter } from "next/navigation";
-import DefautImage from "../../public/asset/avatar2.jpg";
+import DefautImage from "../../../public/asset/avatar2.jpg";
 import Image from "next/image";
 import { useState } from "react";
-import { isBrowser } from "@/utils/isBrowser";
+import { isBrowser } from "../../utils/isBrowser";
 
 // import { SearchBar } from "./SearchBar";
 
