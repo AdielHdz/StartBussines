57ecb754f9e178721c1def36bee30115cb0bf496
valentinavillegasc <116031609+valentinavillegasc@users.noreply.github.com>
2023-07-29 17:59:50 -0500

    edicionDeDatos

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,8 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { useRouter } from "next/navigation";
 import { signOut } from "next-auth/react";
-
+import DefaultImage from "public/asset/avatar2.jpg";
+import Image from "next/image";
 // import { SearchBar } from "./SearchBar";
 
 const navigation = [
@@ -107,10 +108,10 @@ export default function Navbar() {
                   <div>
                     <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
-                      <img
+                      <Image
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                        src={DefaultImage}
+                        alt="avatar"
                       />
                     </Menu.Button>
                   </div>
