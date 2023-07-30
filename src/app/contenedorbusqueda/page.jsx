5142c7d1526cd4db6ddfb068d256f670deb50b78
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-07-29 22:30:07 -0300

    Contenedor de busqueda trae info del back

--- src/app/contenedorbusqueda/page.jsx
+++ src/app/contenedorbusqueda/page.jsx
@@ -3,26 +3,25 @@
 import React from "react";
 import DefaultRating from "../../Components/Rating/Rating";
 import ArticleCard from "./articlesData/page";
-
+import { useState, useEffect } from "react";
 
 const ContenedorBusquedaCard = () => {
-  const articlesData = [
-    {
-      title: "MACHINE IA",
-      imageSrc: "https://unsplash.it/id/1/640/425",
-      totalAmount: "$50,000",
-      minInvestment: "$1,500",
-      maxInvestment: "$5,000",
-    },
-    {
-      title: "ROBOT",
-      imageSrc:
-        "https://images.unsplash.com/photo-1546776310-eef45dd6d63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=810&q=80",
-      totalAmount: "$60,000",
-      minInvestment: "$2,000",
-      maxInvestment: "$7,000",
-    },
-  ];
+  const [articlesData, setArticlesData] = useState([]);
+
+  // Función para hacer la solicitud a la API y obtener los datos
+  const fetchArticlesData = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/projects");
+      const data = await response.json();
+      setArticlesData(data);
+    } catch (error) {
+      console.error("Error fetching data from API:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticlesData();
+  }, []);
 
   return (
     <main className="flex flex-col space-y-4 min-h-screen w-full items-center justify-center p-4 sm:m-2 lg:m-4">
@@ -37,5 +36,3 @@ const ContenedorBusquedaCard = () => {
 };
 
 export default ContenedorBusquedaCard;
-
-
