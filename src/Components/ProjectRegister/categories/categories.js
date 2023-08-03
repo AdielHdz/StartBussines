fa2467cafc549662620f2e19e5178eae91d84877
Daniels35 <danielstiven35@gmail.com>
2023-08-03 17:52:44 -0500

    Componente crear proyecto terminado

---
+++ src/Components/ProjectRegister/categories/categories.js
@@ -0,0 +1,19 @@
+const categories = [
+    "Art",
+    "Comics",
+    "Crafts",
+    "Dance",
+    "Design",
+    "Fashion",
+    "Film & Video",
+    "Food",
+    "Games",
+    "Journalism",
+    "Music",
+    "Photography",
+    "Publishing",
+    "Technology",
+    "Theater",
+  ];
+
+  export default categories;
