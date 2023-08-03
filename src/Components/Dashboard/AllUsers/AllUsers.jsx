aea4446312297f01227c057aef1b33fab8c50017
Ramiro Fassetta <ramiro.fassetta.01@gmail.com>
2023-08-03 18:03:55 -0300

    dashboard

--- src/Components/Dashboard/AllUsers/AllUsers.jsx
+++ src/Components/Dashboard/AllUsers/AllUsers.jsx
@@ -6,7 +6,7 @@ const AllUsers = ({ users, onUserClick }) => {
       <h2 className="text-lg font-semibold mb-4">All Users</h2>
       <ul>
         {users.map((user) => (
-          <li key={user.id} onClick={() => onUserClick(user)} className="cursor-pointer">
+          <li key={user.id} onClick={() => onUserClick(user)} className="cursor-pointer mb-2 border border-gray-400 rounded-lg px-4 py-2 hover:text-blue-500">
             {user.fullName} ({user.id})
           </li>
         ))}
