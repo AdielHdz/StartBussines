f0bdf3ec277d30900d8b0291d6dd0b8ae18d6473
Adiel <wereber.dragons14@live.com.mx>
2023-08-04 12:16:59 -0600

    Validaciones myOpinion

--- src/Components/TextPair/TextPair.jsx
+++ src/Components/TextPair/TextPair.jsx
@@ -1,10 +1,8 @@
 const TextPair = ({ text1, text2, textArr }) => {
   return (
-    <div>
-      <h5 className="inline-block text-base mb-0 mr-1 font-regular ">
-        {text1}
-      </h5>
-      <p className="inline text-xs text-darkViolet font-bold">{text2}</p>
+    <div className="flex items-center ">
+      <h5 className="inline-block text-sm mb-0 mr-1 font-medium">{text1}</h5>
+      <p className="inline text-xs text-darkViolet font-medium">{text2}</p>
     </div>
   );
 };
