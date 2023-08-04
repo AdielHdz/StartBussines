49e04afc67bd7a47764b6f931853381e3e93a598
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-08-03 22:24:32 -0300

    Paginado final

--- src/Components/paginator/Paginator.jsx
+++ src/Components/paginator/Paginator.jsx
@@ -1,95 +1,12 @@
-"use client";
-import React from 'react';
-import {useState} from 'react';
+import React from "react";
 
-
-const Pagination = () => {
-  /*const Pagination = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-  
-    const handlePageClick = (page) => {
-      // Aquí puedes realizar cualquier acción que necesites al cambiar de página,
-      // por ejemplo, cargar nuevos datos o actualizar la vista.
-      setCurrentPage(page);
-    };
-*/
+const Pagination = ({ currentPage }) => {
   return (
-    <>
-      <ol className="flex justify-center gap-1 text-xs font-medium">
-        <li>
-          <a
-            href="#"
-            className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
-          >
-            <span className="sr-only">Prev Page</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-3 w-3"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="#"
-            className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
-          >
-            1
-          </a>
-        </li>
-
-        <li className="block h-8 w-8 rounded border-blue-600 bg-blue-600 text-center leading-8 text-white">
-          2
-        </li>
-
-        <li>
-          <a
-            href="#"
-            className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
-          >
-            3
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="#"
-            className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
-          >
-            4
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="#"
-            className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
-          >
-            <span className="sr-only">Next Page</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-3 w-3"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
-        </li>
-      </ol>
-    </>
+    <div className="flex items-center justify-center mt-4">
+      <span className="px-4 py-2 rounded-md bg-teal-600 text-white font-bold">
+        {currentPage}
+      </span>
+    </div>
   );
 };
 
--- src/app/contenedorbusqueda/page.jsx
+++ src/app/contenedorbusqueda/page.jsx
@@ -10,7 +10,8 @@ import { fetchArticlesData } from "../../Redux/Fetching/Projects/ProjectSlice";
 import { ordered } from "../../Redux/Fetching/Filters/FiltersSlice";
 import { FaArrowLeft } from "react-icons/fa";
 import Link from "next/link";
-// import { Pagination } from "../../Components/Paginator/Paginator";
+import PaginationLogic from "../../Components/paginator/PaginatorLogic";
+import Pagination from "../../Components/Paginator/Paginator";
 
 const ContenedorBusquedaCard = () => {
   const filters = useSelector((state) => state.filters);
@@ -60,11 +61,28 @@ const ContenedorBusquedaCard = () => {
     dispatch(ordered(e.target.value));
   };
 
+  //NOTE - Paginación
+  const [currentPage, setCurrentPage] = useState(1);
+  const projectsPerPage = 5;
+
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = articlesData.slice(
+    indexOfFirstProject,
+    indexOfLastProject
+  );
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+    window.scrollTo(0, 0); 
+  };
+
   return (
     <div>
       <Link href="/home">
         <button className="absolute flex items-center right-0 md:right-60 md:ml-2 bg-teal-700 text-white py-2 px-4 rounded-3xl shadow">
-          <FaArrowLeft /><span className="ml-2">Home</span>
+          <FaArrowLeft />
+          <span className="ml-2">Home</span>
         </button>
       </Link>
       <div className="flex flex-col left-0 items-center justify-center mt-3">
@@ -80,23 +98,36 @@ const ContenedorBusquedaCard = () => {
         />
       </div>
       <div className="flex flex-col items-center justify-center">
-        <h2 className="mb-2 text-lg font-medium text-gray-900 dark:text-gray-300 mt-2">Order: </h2>
-        <select name="orderByRating" onChange={handleOrder}
-                className="px-4 py-2 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-md shadow-sm focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
-                >
+        <h2 className="mb-2 text-lg font-medium text-gray-900 dark:text-gray-300 mt-2">
+          Order:{" "}
+        </h2>
+        <select
+          name="orderByRating"
+          onChange={handleOrder}
+          className="px-4 py-2 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-md shadow-sm focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
+        >
           <option value="Desc">Best ratings</option>
           <option value="Asc">Worst ratings</option>
         </select>
       </div>
       <div className="flex flex-col space-y-4 min-h-screen w-full items-center justify-center p-4 sm:m-2 lg:m-4">
         <div className="flex items-start m-4 text-xl text-black lg:text-2xl">
-          {articlesData?.length} <span className="text-2xl text-teal-700 font-semibold ml-2">Results</span>
+        {currentProjects?.length}{" "}
+          <span className="text-2xl text-teal-700 font-semibold ml-2">
+            Results
+          </span>
         </div>
-        {articlesData.map((article, index) => (
+        {currentProjects.map((article, index) => (
           <ArticleCard key={index} data={article} />
         ))}
       </div>
-     {/*<Pagination /> */}      
+      {/*<Pagination /> */}
+      <Pagination currentPage={currentPage} />
+      <PaginationLogic
+        totalItems={articlesData.length}
+        itemsPerPage={projectsPerPage}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 };
