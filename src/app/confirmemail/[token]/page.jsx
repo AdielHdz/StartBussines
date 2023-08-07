92af4a80fa4dfab9f2e3f36c077fcc009ec309b6
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-08-07 14:55:16 -0300

    SendMail

--- src/app/confirmemail/[token]/page.jsx
+++ src/app/confirmemail/[token]/page.jsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import axios from "axios";
+import { useNavigation , useParams } from "next/router";
+
 
 
 const ConfirmEmailSuccess = () => {
-  const router = useRouter();
-  const { token } = router.query;
+  const navigation = useNavigation();
+  const { token } = useParams();
   const [loading, setLoading] = useState(true);
   const [success, setSuccess] = useState(false);
 
@@ -21,24 +22,34 @@ const ConfirmEmailSuccess = () => {
       }
       setLoading(false);
     };
+
     if (token) {
       fetchData();
     }
   }, [token]);
 
   const handleBackToRegistration = () => {
-    window.location.href = "/register";
+    navigation.push("/register");
   };
+
+  const successMessage = "Email Confirmed Successfully";
+  const errorMessage = "Email Confirmation Failed";
+  const successText = "Congratulations! Your email has been successfully confirmed.";
+  const errorText = "Something went wrong with the email confirmation. Please try again later.";
 
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-lg">
-        <h1 className={`text-center text-2xl font-bold ${success ? 'text-green-600' : 'text-red-600'} sm:text-3xl`}>
-          {success ? 'Email Confirmed Successfully' : 'Email Confirmation Failed'}
+        <h1
+          className={`text-center text-2xl font-bold ${
+            success ? "text-green-600" : "text-red-600"
+          } sm:text-3xl`}
+        >
+          {success ? successMessage : errorMessage}
         </h1>
 
         <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
-          {success ? 'Congratulations! Your email has been successfully confirmed.' : 'Something went wrong with the email confirmation. Please try again later.'}
+          {success ? successText : errorText}
         </p>
 
         <div className="flex justify-center mt-6">
@@ -58,19 +69,17 @@ const ConfirmEmailSuccess = () => {
               />
             </svg>
           ) : (
-            <button
+            <a
               className="text-white bg-green-600 hover:bg-green-700 px-4 py-2 rounded-md"
-              onClick={handleBackToRegistration}
-              
+              href="/register"
+              onClick={navigation.push("/register")}
             >
               Back to Registration
-            </button>
+            </a>
           )}
         </div>
       </div>
     </div>
   );
 };
-
-
 export default ConfirmEmailSuccess;
